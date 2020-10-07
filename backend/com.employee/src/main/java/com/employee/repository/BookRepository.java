23b3ea6b1610dba16bd453913a481f75b039fbb2
Mahmoud Osman <mahmoudosman.som@gmail.com>
2020-10-07 13:03:55 -0500

    added Book java class modal, respository & and Controller

--- backend/com.employee/src/main/java/com/employee/repository/BookRepository.java
+++ backend/com.employee/src/main/java/com/employee/repository/BookRepository.java
@@ -3,7 +3,5 @@ package com.employee.repository;
 import com.employee.modal.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
-
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
