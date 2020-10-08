533bca1ad6b4bbff14a63780924958d629da1894
Mahmoud Osman <mahmoudosman.som@gmail.com>
2020-10-08 13:53:21 -0500

    added employee save method in the backend

--- backend/com.employee/src/main/java/com/employee/modal/Book.java
+++ backend/com.employee/src/main/java/com/employee/modal/Book.java
@@ -1,13 +1,17 @@
 package com.employee.modal;
 
-
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "books")
 public class Book {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "title")
