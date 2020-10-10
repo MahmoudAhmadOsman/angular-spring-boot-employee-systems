178e3b7f3f79e07103d1df2418aed4d80468cb7b
Mahmoud Osman <mahmoudosman.som@gmail.com>
2020-10-10 13:47:27 -0500

    added update & save methods in the controller Spring Book backend

--- backend/com.employee/src/main/java/com/employee/controller/EmployeeController.java
+++ backend/com.employee/src/main/java/com/employee/controller/EmployeeController.java
@@ -1,12 +1,27 @@
 package com.employee.controller;
 
+import com.employee.exception.ResourceNotFoundException;
 import com.employee.modal.Employee;
 import com.employee.repository.EmployeeRepository;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+ import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+ import org.springframework.web.bind.annotation.RestController;
+
+
 
 //Add RestController  & RequestMapping Annotations
 @CrossOrigin("http://localhost:4200")
@@ -16,7 +31,7 @@ public class EmployeeController {
 
     //Now, Inject the Employee Repository here & add Autowired annotation
     @Autowired
-    private EmployeeRepository  employeeRepository;
+    private EmployeeRepository employeeRepository;
 
     //Then, setup all the endpoint REST APIs here
 
@@ -35,6 +50,41 @@ public class EmployeeController {
 
         return employeeRepository.save(employee);
     }
+
+   //Get Employee by id rest api by its id
+  @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Employee with this" + id + "does not exist."));
+        //Use ResponseEntity and pass <Employee> as an arg
+        return ResponseEntity.ok(employee);
+
+  }
+
+
+
+   //Update employee using rest api by its id
+    //Annotate @PutMapping,  add @PathVariable &  @RequestBody
+   @PutMapping("/employees/{id}")
+   public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails){
+       //Retrieve user from the database
+       //employee & and employeeDetails are local variables
+       Employee employee = employeeRepository.findById(id)
+               .orElseThrow(()-> new ResourceNotFoundException("Employee with this" + id + "does not exist."));
+       //Now, set the names such as firstName, lastName & email
+       employee.setFirstName(employeeDetails.getFirstName());
+       employee.setLastName(employeeDetails.getLastName());
+       employee.setEmail(employeeDetails.getEmail());
+       //Now save the updated value into the database
+       Employee updatedEmployee = employeeRepository.save(employee);
+       //Now return ok status using ResponseEntity.ok() method
+       return ResponseEntity.ok(updatedEmployee);
+
+   }
+
+
+    //Delete employee using rest api by its id
+
 
 
 }
