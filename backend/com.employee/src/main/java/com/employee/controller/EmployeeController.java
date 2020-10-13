046e79bedd0127d4717bb96218d905dd811b526c
Mahmoud Osman <mahmoudosman.som@gmail.com>
2020-10-13 01:34:40 -0500

    added delete method in @EmployeeController class

--- backend/com.employee/src/main/java/com/employee/controller/EmployeeController.java
+++ backend/com.employee/src/main/java/com/employee/controller/EmployeeController.java
@@ -1,18 +1,24 @@
 package com.employee.controller;
 
-import com.employee.exception.ResourceNotFoundException;
-import com.employee.modal.Employee;
-import com.employee.repository.EmployeeRepository;
-import lombok.extern.java.Log;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.DeleteMapping;
-
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import com.employee.exception.ResourceNotFoundException;
+import com.employee.modal.Employee;
+import com.employee.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 
 //Add RestController  & RequestMapping Annotations
@@ -39,7 +45,6 @@ public class EmployeeController {
     //Annotate @PostMapping to handle the post method
     @PostMapping("/employees") // Map the url to employees & add @RequestBody as an arg to the createEmployee method
     public Employee createEmployee(@RequestBody Employee employee){
-
         return employeeRepository.save(employee);
     }
 
@@ -79,12 +84,12 @@ public class EmployeeController {
     @DeleteMapping("/employees/{id}")
     public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id){
         Employee employee = employeeRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Employee with id  does not exist" + id));
+                .orElseThrow(()-> new ResourceNotFoundException("Employee with this " + id + " does not exist."));
+
         employeeRepository.delete(employee);
         Map<String, Boolean> response = new HashMap<>();
-        response.put("Deleted", Boolean.TRUE);
+        response.put("deleted", Boolean.TRUE);
         return ResponseEntity.ok(response);
-
     }
 
 
