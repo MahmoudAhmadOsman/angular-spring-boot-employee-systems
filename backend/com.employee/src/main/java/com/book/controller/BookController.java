4db625384c2f7350b6de8bb838de4a55b77c1fa1
Mahmoud Osman <mahmoudosman.som@gmail.com>
2020-10-27 01:10:38 -0500

    added phone field in the frontend & java class in the backend

--- backend/com.employee/src/main/java/com/book/controller/BookController.java
+++ backend/com.employee/src/main/java/com/book/controller/BookController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("/api/v2/books/")
+@RequestMapping("/api/e1/books/")
 public class BookController {
 
     //Autowires BookRepository here
--- backend/com.employee/src/main/java/com/employee/repository/EmployeeRepository.java
+++ backend/com.employee/src/main/java/com/employee/repository/EmployeeRepository.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 //CRUD - Repository
-//@CrossOrigin(maxAge = 3600)
+//@CrossOrigin(origins = "http://localhost:4200")
 public interface EmployeeRepository  extends JpaRepository<Employee, Long> {
 }
