de71b8afb34a4615a75b0365e105e7d5f21ef02f
Mahmoud Osman <mahmoudosman.som@gmail.com>
2020-10-27 00:16:55 -0500

    added book modal, controller & repository

--- backend/com.employee/src/main/java/com/employee/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.employee.repository;
-
-import com.employee.modal.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
--- frontend/src/app/models/book.ts
+++ frontend/src/app/models/book.ts
@@ -1,7 +1,7 @@
 export class Book {
     id: number;
     title: string;
-    bookimgUrl: string;
+    bookImage: string;
     ISBN: string;
     description: string;
     price: string;
