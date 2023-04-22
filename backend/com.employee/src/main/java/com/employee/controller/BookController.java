ca85f4955ba599992fe25fc085be77740710a057
Mahmoud Osman <34916796+MahmoudAhmadOsman@users.noreply.github.com>
2023-04-22 21:05:02 +0300

    Update BookController.java
    Update api version

--- backend/com.employee/src/main/java/com/employee/controller/BookController.java
+++ backend/com.employee/src/main/java/com/employee/controller/BookController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v2/")
 public class BookController {
 
     @Autowired
